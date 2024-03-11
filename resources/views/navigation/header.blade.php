8c4f0951209e7282f05d4c81e288a29da336cb63
vladyslavlopatin <ilopatin3@gmail.com>
2024-03-11 19:05:06 +0100

    fix typo

--- resources/views/navigation/header.blade.php
+++ resources/views/navigation/header.blade.php
@@ -68,7 +68,7 @@
     <script>
         $(document).ready(function() {
             $('[data-header-link-id]').each(function (index, element) {
-                if(! $('#' + $(element).data('header-link-id')).length ) {
+                if(! $('#' + $(element).data('header-link-id')).length) {
                     $(element).css('display', 'none');
                 }
             });
