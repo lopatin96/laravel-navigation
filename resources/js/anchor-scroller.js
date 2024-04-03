20f6368e686d5ed2025b79db22344f84f49833c9
vladyslavlopatin <ilopatin3@gmail.com>
2024-04-03 18:52:14 +0200

    vanila animation

--- resources/js/anchor-scroller.js
+++ resources/js/anchor-scroller.js
@@ -1,9 +1,15 @@
-$(document).on('click', "a[href^='#']", function(e) {
-    e.preventDefault();
+document.addEventListener('click', function(event) {
+    if (event.target.tagName === 'A' && event.target.getAttribute('href').startsWith('#')) {
+        event.preventDefault();
 
-    let target = $($(this).attr('href'));
+        let target = document.querySelector(event.target.getAttribute('href'));
+        let windowHeight = window.innerHeight;
+        let targetOuterHeight = target.offsetHeight;
+        let scrollOffset = parseInt((windowHeight - targetOuterHeight) / 2, 10);
 
-    $('html, body').animate({
-        scrollTop: target.offset().top - parseInt((window.innerHeight - target.outerHeight()) / 2, 10)
-    }, 300);
+        window.scrollTo({
+            top: target.offsetTop - scrollOffset,
+            behavior: 'smooth',
+        });
+    }
 });
